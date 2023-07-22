548e323377670cef762ed1938dff5c1cad4351bd
waseem <waseemazmy21@gmail.com>
2023-07-22 23:51:45 +0300

    Add the handlers for the two buttons in the MovieCard component

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
+import useGlobalContext from '../context/useGlobalContext';
+import ACTIONS from '../context/actions';
 
 const SearchCard = ({ movie }) => {
+  const { dispatch } = useGlobalContext();
+
   return (
     <div className='flex flex-col bg-base-100 shadow-xl rounded-md overflow-hidden'>
       <figure>
@@ -14,8 +18,32 @@ const SearchCard = ({ movie }) => {
         <h2 className='text-xl mb-2'>{movie.Title}</h2>
         <p className='text-sm mb-4'>{movie.Year}</p>
         <div className=' grid grid-cols-[1fr_1fr] gap-1 mt-auto'>
-          <button className='btn'>Add to WatchList</button>
-          <button className='btn'>Add to Watched</button>
+          <button
+            onClick={() => {
+              dispatch({
+                type: ACTIONS.ADD_MOVIE_TO_WATCHLIST,
+                payload: {
+                  movie,
+                },
+              });
+            }}
+            className='btn'
+          >
+            Add to WatchList
+          </button>
+          <button
+            onClick={() => {
+              dispatch({
+                type: ACTIONS.ADD_MOVIE_TO_WATCHED,
+                payload: {
+                  movie,
+                },
+              });
+            }}
+            className='btn'
+          >
+            Add to Watched
+          </button>
         </div>
       </div>
     </div>
